3856ca8e398aa9995110cd5262c677a4f9247f07
heydanhey <danhey@gmail.com>
2016-01-19 21:23:13 -0600

    Class exercise, Model Methods

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,15 @@
 class Employee < ActiveRecord::Base
+  # add methods here
+
+  def friendly_updated_at
+    updated_at.strftime("%b %d, %Y")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def add_japanese_prefix
+    "+81 #{phone_number}"
+  end
 end
